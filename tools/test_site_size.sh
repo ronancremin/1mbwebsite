2c118eff4f4eb1f9e7af1ff0f8747ba447bbfa53
Ronan Cremin <ronan@cremin.com>
2016-03-25 14:48:39 +0000

    Tweaks to make it work live

---
+++ tools/test_site_size.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat site_manifest_live.txt | xargs -n 1 -I {} curl --header "Accept-Encoding: gzip,deflate" --write-out "%{size_download} %{size_header} \n" --output /dev/null -silent {} | awk '{sum = sum + $1 + $2} END {print "Total page size: " sum" bytes, " sum/1048576 "MB"}'
