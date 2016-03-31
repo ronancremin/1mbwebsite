be26ca72bfcc8a3a1c9126b07dcefa81387dfa79
Ronan Cremin <ronan@cremin.com>
2016-03-31 11:31:35 +0100

    Updated test script to give more details

--- tools/test_site_weight.sh
+++ tools/test_site_weight.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-echo "Payload Headers"
-cat site_manifest_live.txt | xargs -n 1 -I {} curl --header "Accept-Encoding: gzip,deflate" --write-out "%{size_download} %{size_header} \n" --output /dev/null -silent {} | awk '{sum = sum + $1 + $2} {print $1, $2} END {print "Total page size: " sum" bytes, " sum/1048576 "MB"}'
+echo "Payload\tHeaders\tTotal\tCode\tFinal URL"
+
+cat site_manifest_live.txt \
+| xargs --max-args=1 -I {} \
+  curl  --header "Accept-Encoding: gzip,deflate" \
+        --write-out "%{size_download} %{size_header} %{http_code} %{url_effective}\n"  \
+        --output /dev/null \
+        --silent {} \
+| awk '
+    {
+        sum = sum + $1 + $2 
+        print $1 "\t" $2 "\t" $1+$2 "\t" $3 "\t" $4
+    } 
+    END {
+        print "\nTotal page transfer weight including headers: " sum" bytes, " sum/1048576 " MB"
+        print "Desired size is: " 1024 * 1024 " bytes"
+        print "Shortfall is: " 1024 * 1024 - sum " bytes"
+    }
+'
